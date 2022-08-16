05c4bdd2bacd9e3a63501a4be30f6035c33cb6c6
Rob Martin <rmartin3@alumni.unca.edu>
2022-08-16 19:03:04 -0400

    initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Post({ post }) {
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: "blocking", // can also be true or 'blocking'
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await res.json();
+  return {
+    props: { post },
+  };
+}
